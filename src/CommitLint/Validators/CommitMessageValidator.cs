4f25fd856572468d5eb7a9dde5d1dbaa1b94b3e0
Lauan Guermandi <lauan.guermandi@stone.com.br>
2025-02-07 12:24:18 -0300

    feat: add ci tag to commit validator

--- src/CommitLint/Validators/CommitMessageValidator.cs
+++ src/CommitLint/Validators/CommitMessageValidator.cs
@@ -4,7 +4,7 @@ namespace CommitLint.Validators;
 
 public partial class CommitMessageValidator : ICommitMessageValidator
 {
-    [GeneratedRegex(@"^(feat|fix|docs|style|refactor|test|chore)(\([\w\-]+\))?: .+$", RegexOptions.Compiled)]
+    [GeneratedRegex(@"^(feat|fix|docs|ci|style|refactor|test|chore)(\([\w\-]+\))?: .+$", RegexOptions.Compiled)]
     private static partial Regex CommitMessageRegex();
 
     public bool Validate(string commitMessage) => CommitMessageRegex().IsMatch(commitMessage);
